db2953c6ecf4bd427cb6057bde1ac90619e5582a
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-07-09 15:27:42 -0500

    Add files via upload

---
+++ Unidad4/Ejercicio2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 15:24:18 2022
+
+@author: LENOVO
+"""
+
+numero_inicial = 1
+numero_final = 16
+
+for i in range (numero_inicial, numero_final):
+    if i % 2 != 0:
+        print(i)
+        
+        
