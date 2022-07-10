324063cc994d0b28ac83c3df8e8fc3fb8c1d3533
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-07-10 15:09:06 -0500

    Add files via upload

---
+++ Unidad5/Ejercicio2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 10 14:57:40 2022
+
+@author: LENOVO
+"""
+
+def numeroprimo(numero):
+    for n in range(2,numero):
+        if numero % n == 0:
+            return False
+        return True
+    
+num = int(input("Ingrese un numero..."))
+if numeroprimo(num):
+    print("El numero es primo")
+else:
+    print("El numero no es primo")
+    
