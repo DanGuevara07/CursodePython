7048c91771379b08b56d6d84b7a2a49e9a4ec588
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-07-15 11:26:19 -0500

    Add files via upload

---
+++ Unidad7/ejercicio1/ejercicio1.py
@@ -0,0 +1,7 @@
+import operaciones
+
+print(operaciones.suma(3,5))
+print(operaciones.resta(6,2))
+print(operaciones.multiplicacion(5,5))
+print(operaciones.division(10,0))
+print(operaciones.division(5,2))
---
+++ Unidad7/ejercicio1/operaciones.py
@@ -0,0 +1,12 @@
+def suma(a,b):
+    return a+b
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b
+def division(a,b):
+    if b==0:
+        print("Error no se puede dividir sobre cero")
+        return None
+    else:
+        return a/b
