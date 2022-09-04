7e38b152822344ff22795d0364b0a780883dc074
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-04 16:44:48 -0500

    Add files via upload

---
+++ Unidad8/Ejercicio2/Ejercicio2.py
@@ -0,0 +1,21 @@
+
+import pickle
+class Vehiculo:
+    def __init__(self, Marca, Color):
+            self.Marca =  Marca
+            self.Color =  Color
+    def __str__(self):
+        return f"Soy un vehiculo de marca {self.Marca} y color {self.Color}"
+
+carro =Vehiculo('Honda', 'Rojo')
+print(carro)
+file = open('carro.dat','wb+')
+pickle.dump(carro, file)
+file.close()
+
+file2 = open('carro.dat','rb')
+carro2= pickle.load(file2)
+file2.close()
+print(carro2)
+
+
