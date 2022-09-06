2f14359f188f8550445fd30153d2212eb91306f1
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-06 17:35:35 -0500

    Add files via upload

---
+++ Unidad10/Ejercicio1.py
@@ -0,0 +1,25 @@
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=3)
+
+def mifuncion():
+    seleccionado.set(None)
+
+seleccionado= tkinter.StringVar()
+r1= ttk.Radiobutton(window, text='opcion1', value= '1', variable=seleccionado)
+r2= ttk.Radiobutton(window, text='opcion2', value= '2', variable=seleccionado)
+r3= ttk.Radiobutton(window, text='opcion3', value= '3', variable=seleccionado)
+
+r1.grid(column=1, row=0,sticky=tkinter.W, pady=5, padx=5)
+r2.grid(column=1, row=1,sticky=tkinter.W, pady=5, padx=5)
+r3.grid(column=1, row=2,sticky=tkinter.W, pady=5, padx=5)
+
+botonReinicio = ttk.Button(window, text="Reiniciar Selección", command=mifuncion)
+botonReinicio.grid(column=1,row=3,sticky=tkinter.W, pady=5,padx=5)
+
+
+
+window.mainloop()
