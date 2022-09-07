129bee442adf5125c474e84004a85c02d1231a9c
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-07 11:54:06 -0500

    Update Ejercicio1.py

--- Unidad4/Ejercicio1.py
+++ Unidad4/Ejercicio1.py
@@ -4,6 +4,10 @@ Created on Sat Jul  9 14:52:40 2022
 
 @author: LENOVO
 """
+"""
+Escribe un programa que pregunte al usuario 
+su edad y muestre por pantalla si es mayor de edad o no.
+"""
 
 edad = int(input("Hola usuario, cual es su edad?"))
 
