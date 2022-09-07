5b3c67120cb443624bf8016c106dd48f007b3fab
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-07 12:04:11 -0500

    Update Ejercicio2.py

--- Unidad8/Ejercicio2/Ejercicio2.py
+++ Unidad8/Ejercicio2/Ejercicio2.py
@@ -1,4 +1,8 @@
-
+"""
+En este segundo ejercicio, tendréis que crear un archivo py 
+y dentro crearéis una clase Vehículo, haréis un objeto de ella, 
+lo guardaréis en un archivo y luego lo cargamos.
+"""
 import pickle
 class Vehiculo:
     def __init__(self, Marca, Color):
