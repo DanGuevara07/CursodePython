48c528e84f0e24f182a9d6fee713a7d290b503d4
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-07 12:03:41 -0500

    Update Ejercicio1.py

--- Unidad8/Ejercicio1/Ejercicio1.py
+++ Unidad8/Ejercicio1/Ejercicio1.py
@@ -1,4 +1,9 @@
 
+"""
+En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, 
+lo abráis y escribáis dentro del archivo. 
+Para ello, tendréis que acceder dos veces al archivo creado.
+"""
 def main():
     f = open('archivo.txt','w+')
     f.write('Hola')
