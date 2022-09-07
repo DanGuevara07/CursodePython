7a4c49c357631a2202f34e9597a4c1775c5d5f1e
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-07 11:58:05 -0500

    Update Ejercicio2.py

--- Unidad4/Ejercicio2.py
+++ Unidad4/Ejercicio2.py
@@ -3,6 +3,10 @@
 Created on Sat Jul  9 15:24:18 2022
 
 @author: LENOVO
+
+Escribe un programa capaz de mostrar todos los números impares desde un número de inicio y otro final.
+
+Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
 """
 
 numero_inicial = 1
