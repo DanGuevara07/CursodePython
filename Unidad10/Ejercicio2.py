7911026bfc868e7e6b3bddde81af7b9992f02487
DanGuevara07 <108958511+DanGuevara07@users.noreply.github.com>
2022-09-07 12:07:23 -0500

    Update Ejercicio2.py

--- Unidad10/Ejercicio2.py
+++ Unidad10/Ejercicio2.py
@@ -1,3 +1,9 @@
+"""
+En este segundo ejercicio, tendréis que crear una interfaz sencilla la 
+cual debe de contener una lista de elementos seleccionables
+, también debe de tener un label con el texto que queráis.
+"""
+
 import tkinter
 from tkinter import ttk
 
